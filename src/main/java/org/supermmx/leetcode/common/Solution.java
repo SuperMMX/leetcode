cb069b72a43a4fda0b40d6bae747ac7b89a69a7a
Xuan MA <SuperMMX@gmail.com>
2022-10-01 14:45:26 +1000

    Add problem and solution interface and class

---
+++ src/main/java/org/supermmx/leetcode/common/Solution.java
@@ -0,0 +1,37 @@
+package org.supermmx.leetcode.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * A solution to a problem
+ */
+@Getter
+@Setter
+public abstract class Solution {
+    /** The corresponding problem */
+    protected Problem problem;
+
+    /** The solution version */
+    protected int version;
+    /** The solution name */
+    protected String name;
+    /** The solution description */
+    protected String description;
+    /** Some comment */
+    protected String comment;
+
+    /**
+     * The generic method to solve the problem with the input
+     *
+     * @param args the input arguments
+     *
+     * @return the result
+     */
+    public abstract Object solve(Object... args);
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ": " + name;
+    }
+}
