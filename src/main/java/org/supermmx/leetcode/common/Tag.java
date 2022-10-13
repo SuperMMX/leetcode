247371f4e89fe141513410fe5fc80122b0578279
Xuan MA <SuperMMX@gmail.com>
2022-10-13 21:38:41 +1100

    Fix tag DYNAMIC_PROGRAMMING

--- src/main/java/org/supermmx/leetcode/common/Tag.java
+++ src/main/java/org/supermmx/leetcode/common/Tag.java
@@ -4,8 +4,9 @@ package org.supermmx.leetcode.common;
  * Tags constants
  */
 public interface Tag {
+    public static final String DYNAMIC_PROGRAMMING = "dynamic-programming";
+    public static final String MATH = "math";
     public static final String HASH_TABLE = "hash-table";
-    public static final String DYNAIC_PROGRAMMING = "dynamic-programming";
     public static final String SLIDING_WINDOW = "sliding-window";
     public static final String STRING = "string";
 }
